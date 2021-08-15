e752eeded153ea930e1e9d40345d02e682a2be1d
sanamaruno <xwkpm269@gmail.com>
2021-08-15 02:37:54 +0000

    [Add]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a025a282e451559c92860c3beb5a5ac700eced44f35c65ab8a3146706a3971093d4d621c2bb868dbd164276a4eaa3cb8d2c35e72fc53c15016d3abfe6fc49b5f'
